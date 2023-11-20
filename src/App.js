abaecb625b741bc61c8912319549f171395d5826
ALAGESHWARI <142484629+ALAGESHWARI@users.noreply.github.com>
2023-11-20 19:43:33 +0530

    Add files via upload
                                           GRID-DESIGN

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './Header'
+import Catogiries from './catogiries';
+import Card from './card';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className='body'>
+        <Catogiries />
+        <div className="main-body">
+          <Card />
+        </div>
+      </div>
+      <footer>
+        </footer>
+    </>
+  );
+}
+
+export default App;
